0c0ee08d243e671b5fd47b49f62d82e494c47220
Marenthe de Jong <marenthedejong@gmail.com>
2022-03-09 00:29:35 +0100

    Database gekoppeld aan de website. Een class aangemaakt voor Product, een functie geschreven in homecontroller om de producten op te vragen (nog niet voor foto/maat). Verder ook nog op de bestelpagina direct code geschreven om de informatie uit de database op te vragen.

--- Mallady/Controllers/HomeController.cs
+++ Mallady/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
 ﻿using Mallady.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using MySql.Data; 
+using MySql.Data.MySqlClient;
+using Mallady.Database;
 
 namespace Mallady.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
+        private readonly string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110374;Uid=110374;Pwd=inf2021sql;";
+        //private readonly string connectionString = "Server=172.16.160.21;Port=3306;Database=110374;Uid=110374;Pwd=inf2021sql;";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -18,11 +22,53 @@ namespace Mallady.Controllers
         {
             return View();
         }
+        
+        public List<Product> GetProducts()
+        {
+           
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<Product> products = new List<Product>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    { 
+                        Product p = new Product
+                        {
+                            // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Naam = reader["naam"].ToString(),
+                            Ingredienten = reader["ingredienten"].ToString(),
+                            Prijs = Convert.ToInt32(reader["prijs"]),
+                        };
+
+                    // voeg de naam toe aan de lijst met namen
+                    products.Add(p);
+                }
+            }
+         }
+
+         // return de lijst met namen
+         return products;
+        }
+
 
         [Route("Bestelpagina")]
         public IActionResult Bestelpagina()
         {
-            return View();
+            var product = GetProducts();
+            return View(product);
         }
 
         [Route("Contact")]
---
+++ Mallady/Database/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Mallady.Database
+{
+    public class Product
+    {
+            public int Id { get; set; }
+            public string Naam { get; set; }
+            public string Ingredienten { get; set; }
+            public int Prijs { get; set; }
+        }
+    }
+
