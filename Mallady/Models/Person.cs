bcb12e8432634393326e8098d5546353d98d6e08
Marenthe de Jong <marenthedejong@gmail.com>
2022-04-04 10:06:31 +0200

    Error van Person.cs over wachtwoord kan niet null zijn weggewerkt.

--- Mallady/Models/Person.cs
+++ Mallady/Models/Person.cs
@@ -10,7 +10,7 @@ namespace Mallady.Models
         [Required(ErrorMessage = "U bent verplicht uw achternaam in te vullen")]
         public string? Achternaam { get; set; }
 
-        public string? Wachtwoord { get; set; }
+        public string Wachtwoord { get; set; }
 
         [Required(ErrorMessage = "U bent verplicht uw emailadres in te vullen")]
         public string? Email { get; set; }
